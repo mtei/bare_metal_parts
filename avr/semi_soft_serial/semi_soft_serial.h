a50ca475ccc22eff85ff0c685bacf9603f193eac
mtei <2170248+mtei@users.noreply.github.com>
2020-02-24 21:11:36 +0900

    add MIT License

--- avr/semi_soft_serial/semi_soft_serial.h
+++ avr/semi_soft_serial/semi_soft_serial.h
@@ -1,6 +1,32 @@
-/* half-duplex(one-wire) semi-soft AVR serial communication routines */
-#ifndef SEMI_SOFT_SERIAL_H
-#define SEMI_SOFT_SERIAL_H
+/* AVR half-duplex(one-wire) semi-soft asynchronous serial communication routines */
+/* clang-format off */
+#ifndef HALF_DUPLEX_SEMI_SOFT_SERIAL_H
+#define HALF_DUPLEX_SEMI_SOFT_SERIAL_H
+#pragma once
+
+/*
+MIT License
+
+Copyright (c) 2020 Takeshi Ishii
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 #ifdef __cplusplus
 extern "C" {
@@ -24,4 +50,4 @@ void HDSS_SEND_BYTES(const uint8_t *datap, uint16_t datalen, _Bool change_receiv
 }
 #endif
 
-#endif /* SEMI_SOFT_SERIAL_H */
+#endif /* HALF_DUPLEX_SEMI_SOFT_SERIAL_H */
