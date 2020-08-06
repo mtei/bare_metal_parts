550a3646f6b231025346e99a3919cac944b8e85a
mtei <2170248+mtei@users.noreply.github.com>
2020-08-06 20:51:22 +0900

    update rgbled_test.c: support many leds

--- avr/ws2812/test/rgbled_test.c
+++ avr/ws2812/test/rgbled_test.c
@@ -2,6 +2,7 @@
 #include <util/delay.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
 #include "avr_io.h"
 #include "ws2812.h"
 
@@ -61,20 +62,55 @@ void led_blink(int on, int off)
     while( off-- ) { DELAY(1); }
 }
 
+#ifndef LED_NUMS
 #define LED_NUMS 10
-#define G1 10,0,0
-#define R1 0,10,0
-#define B1 0,0,10
-#define W1 20,20,20
-
+#endif
+#define ALT_LED_NUMS 8
+#define G1 50,0,0
+#define R1 0,50,0
+#define B1 0,0,50
+#define W1 30,30,30
 #define K1 0,0,0
-#define G10  G1,G1,G1,G1,G1, G1,G1,G1,G1,G1
-#define R10  R1,R1,R1,R1,R1, R1,R1,R1,R1,R1
-#define B10  B1,B1,B1,B1,B1, B1,B1,B1,B1,B1
-#define W10  W1,W1,W1,W1,W1, W1,W1,W1,W1,W1
-#define K10  K1,K1,K1,K1,K1, K1,K1,K1,K1,K1
-#define GRB  G1, R1, B1
 #define GRBK G1, R1, B1, K1
+
+uint8_t led_buffer[LED_NUMS*3+4*3];
+uint8_t led_black[] = { K1 };
+uint8_t led_white[] = { W1 };
+uint8_t led_green[] = { G1 };
+uint8_t led_red[]   = { R1 };
+uint8_t led_blue[]  = { B1 };
+uint8_t led_grbk[]  = { GRBK, GRBK };
+
+typedef struct led_patterns_t {
+    uint8_t *data;
+    uint8_t size;
+} led_patterns_t;
+
+led_patterns_t led_patterns[] = {
+    { led_black,    sizeof(led_black)  },
+    { led_white,    sizeof(led_white)  },
+    { led_black,    sizeof(led_black)  },
+    { led_green,    sizeof(led_green)  },
+    { led_black,    sizeof(led_black)  },
+    { led_red,      sizeof(led_red)    },
+    { led_black,    sizeof(led_black)  },
+    { led_blue,     sizeof(led_blue)   },
+    { led_black,    sizeof(led_black)  },
+    { led_grbk,     sizeof(led_grbk)/2 },
+    { led_grbk+3,   sizeof(led_grbk)/2 },
+    { led_grbk+3*2, sizeof(led_grbk)/2 },
+    { led_grbk+3*3, sizeof(led_grbk)/2 },
+};
+
+void fill_led_buffer(int index)
+{
+    led_patterns_t *pattern = led_patterns + index;
+    uint8_t *dst, *dstend;
+    dstend = led_buffer + sizeof(led_buffer);
+    for (dst = led_buffer; dst + pattern->size < dstend; dst += pattern->size){
+         memcpy(dst, pattern->data, pattern->size);
+    }
+}
 
 uint8_t testled[][3*3] = {
     { 1, 1, 1, 1, 1, 1, 1, 1, 1 },
@@ -82,29 +118,6 @@ uint8_t testled[][3*3] = {
     { 255, 255, 255, 255, 255, 255, 255, 255, 255 },
     { 0, 0, 0, 0, 0, 0, 0, 0, 0 }
 };
-
-#if LED_NUMS == 10
-uint8_t led[][LED_NUMS*3] = {
-    { K10 },
-    { W10 },
-    { K10 },
-    { G10 },
-    { K10 },
-    { R10 },
-    { K10 },
-    { B10 },
-    { K10 },
-
-    { GRBK, GRBK, R1, B1  },
-    { K1, GRBK, GRBK, R1  },
-    { B1, K1, GRBK, GRBK  },
-    { R1, B1, K1, GRBK, GRB  },
-    { GRBK, GRBK, R1, B1  },
-    { K1, GRBK, GRBK, R1  },
-    { B1, K1, GRBK, GRBK  },
-    { R1, B1, K1, GRBK, GRB  },
-};
-#endif
 
 uint8_t led_run[LED_NUMS*3*2] = {
     [LED_NUMS*3] = 60,    [LED_NUMS*3+1] = 60,    [LED_NUMS*3+2] = 60,
@@ -148,22 +161,26 @@ int main(void)
         uint8_t disp_rgb2[][3] = {
             {255, 0, 255}, {255, 255, 0}
         };
-        for(int i = 0; i < LED_NUMS; i++ ) {
-            ws2812_display_rgb(1<<i, LED_NUMS, true, disp_rgb2[i&1]);
+        _delay_us(WS2812_RESET);
+        for(int i = 0; i < ALT_LED_NUMS; i++ ) {
+            ws2812_display_rgb(1<<i, ALT_LED_NUMS, true, disp_rgb2[i&1]);
             DELAY(200);
         }
-        for(int i = LED_NUMS-1; i >= 0; i-- ) {
-            ws2812_display(1<<i, LED_NUMS, true);
+        for(int i = ALT_LED_NUMS-1; i >= 0; i-- ) {
+            ws2812_display(1<<i, ALT_LED_NUMS, true);
             DELAY(200);
         }
 
-        ws2812_display(++tcount, LED_NUMS, true);
+        ws2812_display(++tcount, ALT_LED_NUMS, true);
         DELAY(2000);
-        for (int i = 0; i < (sizeof(led)/sizeof(led[0])); i++ ) {
+        for (int i = 0; i < (sizeof(led_patterns)/sizeof(led_patterns[0]));
+             i++ ) {
             led_blink(100,900);
-            ws2812_send_bytes_port(led[i], sizeof(led[0]),
+            fill_led_buffer(i);
+            ws2812_send_bytes_port(led_buffer, LED_NUMS*3,
                                    &PORTx(TEST_DI_PIN), _BV(P_BITx(TEST_DI_PIN)));
         }
+        led_blink(100,900);
     }
     return 0;
 }
